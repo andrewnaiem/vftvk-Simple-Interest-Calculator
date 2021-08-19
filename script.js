26098b94da25c0601b144a6683e9afe20f0a0a4e
andrewnaiem <39439452+andrewnaiem@users.noreply.github.com>
2021-08-19 23:37:31 +0200

    Finished

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+
+    p = document.getElementById("principal");
     
+    if (p.value > 0) {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+        document.getElementById('result').innerText = "If you deposit " + principal + "\n, at an interest rate of " + rate + "%.\nYou will receive an amount of " + interest + ",\n in the year " + year;
+    }
+    else {
+        alert("Please enter a postive number");
+        p.focus();
+    }
 }
-        
+
+
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
